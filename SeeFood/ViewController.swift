5df4ad27e560f5196b34613c79176406665ac1b4
Kin Fat WONG <kinfat.wong@6ka.com.hk>
2019-01-19 02:13:15 +0800

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Huanqiang Kin Fat WONG on 19/1/2019.
+//  Copyright © 2019 Huanqiang Kin Fat WONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
